08407dc8ea0322a41bba646f06ab4a7b6c103d67
Lukasz Niezabitowski <lukasznie1320@gmail.com>
2023-05-31 16:37:43 +0100

    homepage (kinda) and better nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,24 +2,78 @@
   // import "../app.css";
   import "carbon-components-svelte/css/white.css";
 
-  import { Content, Header, HeaderNav, HeaderNavItem, SkipToContent } from "carbon-components-svelte";
+  import {
+    Content,
+    Header,
+    HeaderNav,
+    HeaderNavItem,
+    HeaderNavMenu,
+    SideNav,
+    SideNavDivider,
+    SideNavItems,
+    SideNavLink,
+    SkipToContent,
+  } from "carbon-components-svelte";
+  import {
+    Catalog,
+    ChartScatter,
+    CircleDash,
+    Data_2,
+    Fade,
+    Home,
+    LetterGg,
+    LetterSs,
+    QComposerEdit,
+    Select_02,
+    Tree,
+  } from "carbon-icons-svelte";
+
+  let isSideNavOpen = false;
 </script>
 
-<Header company="Ohnologs" platformName="Database">
+<Header bind:isSideNavOpen company="Ohnologs" platformName="Database">
   <svelte:fragment slot="skip-to-content">
     <SkipToContent />
   </svelte:fragment>
+
   <HeaderNav>
-    <HeaderNavItem href="/ohnologs/blast" text="BLAST" />
-    <HeaderNavItem href="/ohnologs/gene" text="Gene" />
-    <HeaderNavItem href="/ohnologs/species" text="Species" />
-    <HeaderNavItem href="/ohnologs/select" text="Select" />
-    <HeaderNavItem href="/ohnologs/circos" text="Circos" />
-    <HeaderNavItem href="/ohnologs/dotplot" text="Dotplot" />
-    <HeaderNavItem href="/ohnologs/synteny" text="Synteny" />
-    <HeaderNavItem href="/ohnologs/tree" text="Tree" />
+    <HeaderNavItem href="/ohnologs" text="Home" />
+    <HeaderNavMenu text="Data">
+      <HeaderNavItem href="/ohnologs/species" text="Species" />
+      <HeaderNavItem href="/ohnologs/gene" text="Genes" />
+    </HeaderNavMenu>
+    <HeaderNavMenu text="Utilities">
+      <HeaderNavItem href="/ohnologs/select" text="Selection" />
+      <HeaderNavItem href="/ohnologs/blast" text="BLAST" />
+    </HeaderNavMenu>
+    <HeaderNavMenu text="Figures">
+      <HeaderNavItem href="/ohnologs/circos" text="Circos" />
+      <HeaderNavItem href="/ohnologs/dotplot" text="Dotplot" />
+      <HeaderNavItem href="/ohnologs/tree" text="Phylogeny" />
+      <HeaderNavItem href="/ohnologs/synteny" text="Synteny" />
+    </HeaderNavMenu>
+    <HeaderNavItem href="/ohnologs/docs" text="Documentation" />
   </HeaderNav>
 </Header>
+
+<SideNav bind:isOpen={isSideNavOpen} rail>
+  <SideNavItems>
+    <SideNavLink icon={Home} href="/ohnologs" text="Home" />
+    <SideNavDivider />
+    <SideNavLink icon={LetterSs} href="/ohnologs/species" text="Species" />
+    <SideNavLink icon={LetterGg} href="/ohnologs/gene" text="Genes" />
+    <SideNavDivider />
+    <SideNavLink icon={Select_02} href="/ohnologs/select" text="Selection" />
+    <SideNavLink icon={Data_2} href="/ohnologs/blast" text="BLAST" />
+    <SideNavDivider />
+    <SideNavLink icon={CircleDash} href="/ohnologs/circos" text="Circos" />
+    <SideNavLink icon={ChartScatter} href="/ohnologs/dotplot" text="Dotplot" />
+    <SideNavLink icon={Tree} href="/ohnologs/tree" text="Phylogeny" />
+    <SideNavLink icon={QComposerEdit} href="/ohnologs/synteny" text="Synteny" />
+    <SideNavDivider />
+    <SideNavLink icon={Catalog} href="/ohnologs/docs" text="Documentation" />
+  </SideNavItems>
+</SideNav>
 
 <Content>
   <slot />
